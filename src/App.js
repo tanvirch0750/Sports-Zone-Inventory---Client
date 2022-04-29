d12e9335977d7970c768694641e57b47f4d218d9
tanvirch0750 <tanvir0750@yahoo.com>
2022-04-30 00:25:11 +0600

    implemented header section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
+import Header from "./shared/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>hello</h1>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link className="logo-link" to="/">
+          sinventory
+        </Link>
+      </div>
+      <ul className="navigation">
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/manage-inventory">
+            Manage Inventory
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link className="btn btn-sm" to="/signup">
+            Signup
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
